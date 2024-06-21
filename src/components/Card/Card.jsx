13468e6408799818a282572f29a8d341c50e727a
giorgi1001 <giorgi1001gio@gmail.com>
2024-06-21 16:07:15 +0400

    image import change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import Description from "../Description/Description";
 import "./Card.css";
+import Hero from "/illustration-hero.svg";
 
 export default function Card() {
   return (
     <div className="card">
-      <img className="hero-img" src="./illustration-hero.svg" alt="hero img" />
+      <img className="hero-img" src={Hero} alt="hero img" />
       <Description />
     </div>
   );
